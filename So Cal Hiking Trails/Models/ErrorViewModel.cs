728527b274e8dd3ed3943021bd6cc5ebf2c09f6f
Edward (Eddie) Pantoja <edward.pantoja@sjsu.edu>
2018-07-19 18:58:09 -0700

    Add project files.

---
+++ So Cal Hiking Trails/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace So_Cal_Hiking_Trails.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
